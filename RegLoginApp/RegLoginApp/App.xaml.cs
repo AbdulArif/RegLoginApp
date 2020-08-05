ae13c7fea94e8db0b6535f1eda0dfe6e85733195
AbdulArif <abdularif.dgp@gmail.com>
2020-08-05 10:23:37 +0530

    Add project files.

---
+++ RegLoginApp/RegLoginApp/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using RegLoginApp.Views;
+using Xamarin.Forms;
+
+namespace RegLoginApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new LoginPage());
+
+            //MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ RegLoginApp/RegLoginApp/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using RegLoginApp.Tables;
+using RegLoginApp.Views;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RegLoginApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            SetValue(NavigationPage.HasNavigationBarProperty, false);
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var dbpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "UserDatabase.db");
+            var db = new SQLiteConnection(dbpath);
+            db.CreateTable<RegUserTable>();
+            var item = new RegUserTable()
+            {
+                UserName = EntryUserName.Text,
+                Password = EntryUserPassword.Text,
+                Email = EntryUserEmail.Text,
+                PhoneNumber = EntryUserPhoneNumber.Text
+            };
+            db.Insert(item);
+            Device.BeginInvokeOnMainThread(async () => {
+                var result = await this.DisplayAlert("Congrats", "User registration successfull", "Yes", "Cancel");
+                if (result)
+                    await Navigation.PushAsync(new LoginPage());
+            });
+        }
+    }
+}
